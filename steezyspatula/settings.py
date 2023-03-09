0cb6fa7fab12c3d00dc7701d73cc4335ecc2a02d
GaborFicsor <ficsorgabor@gmail.com>
2023-03-09 22:38:33 +0000

    Fix ALLOWED_HOSTS Url in settings.py again

--- steezyspatula/settings.py
+++ steezyspatula/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['steezy-spatula.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['steezy-spatula.herokuapp.com', 'localhost']
 
 
 # Application definition
