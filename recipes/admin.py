309b8de839e43c4eb7e69ad76ca3442c04687fa2
GaborFicsor <ficsorgabor@gmail.com>
2023-03-15 20:18:03 +0000

    Update admin.py and migrate summernote to database

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.site.register(Recipe)
-# Register your models here.
+@admin.register(Recipe)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('method', 'ingredients')
