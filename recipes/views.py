b7a25e26d340066d927d784f86335f0a8aac9a9b
GaborFicsor <ficsorgabor@gmail.com>
2023-04-04 19:05:57 +0000

    Add recipes.html template that lists recipes from the database

--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,11 @@ from django.views import generic
 from .models import Recipe
 
 
+class HomeView(generic.TemplateView):
+    template_name = 'home.html'
+
 class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 9
+    template_name = 'recipes.html'
+    paginate_by = 6
--- steezyspatula/settings.py
+++ steezyspatula/settings.py
@@ -32,7 +32,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['steezy-spatula.herokuapp.com', 'localhost']
 
-
 # Application definition
 
 INSTALLED_APPS = [
