e1d495fb49ac0d46ed15d36cbc603eabbefdea84
GaborFicsor <ficsorgabor@gmail.com>
2023-04-04 20:40:29 +0000

    Fix navigation links

--- recipes/views.py
+++ recipes/views.py
@@ -10,4 +10,4 @@ class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'recipes.html'
-    paginate_by = 6
+    paginate_by = 9
